4d0535f7b394a7b1f7a5bfaec3ca0bf19170014a
Elijah Kadimba <ekadimba10@gmail.com>
2020-12-14 19:14:12 +0200

    Master Password must be entered in order to create new accounts

--- Green Enviro App/LoginForm.cs
+++ Green Enviro App/LoginForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace Green_Enviro_App
 {
@@ -29,6 +30,9 @@ namespace Green_Enviro_App
 
         //Creates a single instance of the CreateAccount class
         CreateAccount _account = new CreateAccount();
+
+        //Master Password (Changeable depending on the devs)
+        const string _master_password = "1234";
         
 
         public LoginForm()
@@ -139,9 +143,21 @@ namespace Green_Enviro_App
 
         private void createAccountButton_Click(object sender, EventArgs e)
         {
-            //Creates a new instance everytime the new account button is clicked.
-            _account.Activate();
-            _account.Show();
+            AdminPass();
+        }
+        private void AdminPass()
+        {
+            string content = Interaction.InputBox("Enter Master Password: ", "Administrator", default);
+            if (content == _master_password)
+            {
+                //Creates a new instance everytime the new account button is clicked.
+                _account.Activate();
+                _account.Show();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect master password", "Administrator", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
     }
 }
