d336bee9aa96330c66da1c521ec9286dda295a2c
= <=>
2022-03-23 21:58:20 +0200

    Added page borders and header

--- Green Enviro App/src/Models/MainFormModels/PoliceRegister.cs
+++ Green Enviro App/src/Models/MainFormModels/PoliceRegister.cs
@@ -40,6 +40,7 @@ namespace Green_Enviro_App
 			page.Orientation = PdfSharp.PageOrientation.Landscape;
 			graphic = XGraphics.FromPdfPage(page);
 
+			insertBorder();
 			insertHeader();
 
 			try
@@ -53,6 +54,81 @@ namespace Green_Enviro_App
 			}
 		}
 
+		/// <summary>
+		/// Inserts the page borders.
+		/// </summary>
+		private void insertBorder() 
+		{
+			// Outside borders
+			double start_x = 40;
+			double start_y = 40;
+			double end_x = 800;
+			double end_y = 0;
+			graphic.DrawLine(XPens.Black, new XPoint(start_x, start_y), new XPoint(end_x, start_y));
+
+			start_x = 40;
+			start_y = 40;
+			end_x = 40;
+			end_y = 560;
+			graphic.DrawLine(XPens.Black, new XPoint(start_x, start_y), new XPoint(end_x, end_y));
+
+			start_x = 40;
+			start_y = 560;
+			end_x = 800;
+			end_y = 560;
+			graphic.DrawLine(XPens.Black, new XPoint(start_x, start_y), new XPoint(end_x, end_y));
+
+			start_x = 800;
+			start_y = 40;
+			end_x = 800;
+			end_y = 560;
+			graphic.DrawLine(XPens.Black, new XPoint(start_x, start_y), new XPoint(end_x, end_y));
+
+			// Inside borders
+			start_x = 42;
+			start_y = 42;
+			end_x = 798;
+			end_y = 42;
+			graphic.DrawLine(XPens.Black, new XPoint(start_x, start_y), new XPoint(end_x, end_y));
+
+			start_x = 42;
+			start_y = 42;
+			end_x = 42;
+			end_y = 558;
+			graphic.DrawLine(XPens.Black, new XPoint(start_x, start_y), new XPoint(end_x, end_y));
+
+			start_x = 42;
+			start_y = 558;
+			end_x = 798;
+			end_y = 558;
+			graphic.DrawLine(XPens.Black, new XPoint(start_x, start_y), new XPoint(end_x, end_y));
+
+			start_x = 798;
+			start_y = 42;
+			end_x = 798;
+			end_y = 558;
+			graphic.DrawLine(XPens.Black, new XPoint(start_x, start_y), new XPoint(end_x, end_y));
+		}
+
+		/// <summary>
+		/// Inserts the Police Register header label
+		/// </summary>
+		private void insertHeader()
+		{
+			string heading = "Purchases Police Register";
+			string fontName = "Times New Roman";
+			double fontSize = 25;
+			XFont font = new XFont(fontName, fontSize, XFontStyle.Bold);
+
+			int y_coordinate = 65;
+			int x_coordinate = 290;
+			graphic.DrawString(heading, font, XBrushes.Black, new XPoint(x_coordinate, y_coordinate));
+		}
+
+		/// <summary>
+		/// Generates the path to be used for saving the Police Register pdf files.
+		/// </summary>
+		/// <returns>A string representing the path.</returns>
 		private string savePath() 
 		{
 			string basePath = Constants.POLICE_REGISTERS_PATH;
@@ -65,17 +141,7 @@ namespace Green_Enviro_App
 			return filePath;
 		}
 
-		private void insertHeader() 
-		{
-			string heading = "DELIVERY NOTE";
-			string fontName = "Times New Roman";
-			double fontSize = 25;
-			XFont font = new XFont(fontName, fontSize, XFontStyle.Bold);
 
-			int y_coordinate = 195;
-			int x_coordinate = 187;
-			graphic.DrawString(heading, font, XBrushes.Black, new XPoint(x_coordinate, y_coordinate));
-		}
 
 		/// <summary>
 		/// Creates the police register.
